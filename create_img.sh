1ec0e59135a52caae8ac618f28c3062b3966ae58
ownI <kpranczk7@gmail.com>
2017-07-25 20:26:42 +0200

    Add deploy useful bash scripts

---
+++ create_img.sh
@@ -0,0 +1,16 @@
+virtualenv venv -p python3
+source venv/bin/activate
+git clone https://github.com/theowni/RPI-Disk-Image-Decorator
+cd RPI-Disk-Image-Decorator
+bash setup.sh
+cd src
+# mount raspbian image 
+python3 rpiem.py mount -p ../../raspbian.img
+# setup WiFI
+python3 rpiem.py run_playbook -f ../../playbooks/wifi_playbook.yml -p ../../raspbian.img
+# copy source codes to mounted drive
+sudo cp -r ../../src/racer_soft /mnt/rpiem/
+# install soft
+python3 rpiem.py run_playbook -f ../../playbooks/soft_racer_playbook.yml -p ../../raspbian.img
+# umount raspbian image at finish
+python3 rpiem.py umount
